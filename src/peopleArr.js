ca5f4c1a7e4c84afed9af3fa6488c075e32c6aec
James Sheehan <jmsdevx@gmail.com>
2019-03-15 10:27:11 -0500

    added the real eric

--- src/peopleArr.js
+++ src/peopleArr.js
@@ -1,10 +1,15 @@
 export const people = [
-    {
-        name: 'Tim Biles',
-        weirdFact: 'I have a collection of over 150 different pink sweetener packets.'
-    },
-    {
-        name: 'Jonathan May',
-        weirdFact: 'I think shoes are dumb.'
-    },
+  {
+    name: "Tim Biles",
+    weirdFact:
+      "I have a collection of over 150 different pink sweetener packets."
+  },
+  {
+    name: "Jonathan May",
+    weirdFact: "I think shoes are dumb."
+  },
+  {
+    name: "Eric Vere",
+    weirdFact: "No, I like HATS!!!!!!!!"
+  }
 ];
